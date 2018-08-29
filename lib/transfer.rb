2013766acf480cb1a65ed0abde622132972e5d1d
Achsah Abraham <achsahmp@gmail.com>
2018-08-29 00:48:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,10 @@ class Transfer
   # your code here
   attr_accessor :name, :person, :amount
 
-  def initialize(name)
+  def initialize(name, person, amount)
     @name = name
+    @person = person
+    @amount = amount
+    
   end
 end
